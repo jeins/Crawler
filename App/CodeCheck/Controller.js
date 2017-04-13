118bc849446cba35caf68b4c55ee4e5ac8fe399b
jeins <pinghead123>
2017-04-13 14:04:33 +0200

    refactoring api get product status

--- App/CodeCheck/Controller.js
+++ App/CodeCheck/Controller.js
@@ -6,21 +6,31 @@ const logger = require('../../Helper/Logger');
 const Product = require('./Model');
 const router = express.Router();
 
+const haramIngredients = [
+	'bier', 'rind', 'fleisch', 'wein',
+	'ethylalkohol', 'alkohol', 'schwein', 'gelatine', 'e441',
+	'e921', 'l-cystin', 'e920', 'l-cystein', 'e542', 'knochenphosphate',
+	'e471', 'e471a', 'e471b', 'e471c', 'e471d', 'e471e', 'e471f', 
+	'e422', 'glycerin', 'e407', 'carrageen', 'e392', 'rosmarinextrakt', 
+	'e120', 'cochenille', 'karminsäure'
+];
+
 router.get('/:eanCode', (req, res)=>{
 	let eanCode = req.params.eanCode;
-	let result = 'eanCode title ingredient imageUrl';
 
-	Product.findOne({eanCode: eanCode}, result, (err, product)=>{
+	Product.findOne({eanCode: eanCode}, (err, product)=>{
 		if(err) res.status(500).send(err);
 		else if(product){
-			let productIngredientStatus = checkProduct(product.ingredient);
+			let productStatus = checkProductStatus(product);
 			let result = {
 				eanCode: product.eanCode,
 				title: product.title,
 				imageUrl: product.imageUrl,
-				ingredient: productIngredientStatus.ingredient,
-				status: productIngredientStatus.status
+				ingredient: productStatus.ingredient,
+				status: productStatus.status
 			};
+
+			if(productStatus.haramCategory) result.haramCategory = productStatus.haramCategory;
 
 			res.json(result);
 		} else{
@@ -29,16 +39,24 @@ router.get('/:eanCode', (req, res)=>{
 	});
 });
 
-function checkProduct(ingredient){
-	let status = 'hallal';
-	let haramIngredients = [
-		'bier', 'rind', 'fleisch',
-		'ethylalkohol', 'alkohol', 'schwein', 'gelatine', 'e441',
-		'e921', 'l-cystin', 'e920', 'l-cystein', 'e542', 'knochenphosphate',
-		'e471', 'e471a', 'e471b', 'e471c', 'e471d', 'e471e', 'e471f', 
-		'e422', 'glycerin', 'e407', 'carrageen', 'e392', 'rosmarinextrakt', 
-		'e120', 'cochenille', 'karminsäure'
+function checkProductStatus(prod){
+	let result = {status: 'hallal', ingredient: ''};
+	let checkHandler = [
+		checkProductByIngredients(prod.ingredient),
+		checkProductByCategoryLv(prod)
 	];
+
+	_.forEach(checkHandler, (val)=>{
+		if(val.status === 'haram') result.status = val.status;
+		if(val.ingredient) result.ingredient = val.ingredient;
+		if(val.haramCategory) result.haramCategory = val.haramCategory;
+	});
+
+	return result;
+}
+
+function checkProductByIngredients(ingredient){
+	let status = 'hallal';
 	ingredient = ingredient.toLowerCase();
 
 	_.forEach(haramIngredients, (haramIngredient)=>{
@@ -49,6 +67,26 @@ function checkProduct(ingredient){
 	});
 
 	return {ingredient: ingredient, status: status};
+}
+
+function checkProductByCategoryLv(prod){
+	let categoryLv = ['firstLvCategory', 'secondLvCategory', 'thirdLvCategory'];
+	let status = 'hallal';
+	let haramCat = '';
+
+	_.forEach(categoryLv, (cat)=>{
+		if(prod[cat]){
+			_.forEach(haramIngredients, (haramIngredient)=>{
+				if(_.includes(prod[cat], haramIngredient)){
+					status = 'haram';
+					haramCat = haramIngredient;
+					return false;
+				}
+			});
+		}
+	});
+
+	return {status: status, haramCategory: haramCat};
 }
 
 function highlightHaramIngredients(ingredient, haramIngredient){
