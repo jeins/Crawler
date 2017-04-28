91905b17ac80032eb0fd751f403c130e184b5702
Muhammad Juan Akbar <mail@mjuan.me>
2017-04-28 21:13:07 +0200

    finishing add murottal

--- App/Murottal/Controller.js
+++ App/Murottal/Controller.js
@@ -5,6 +5,8 @@ const _ = require('lodash');
 const router = express.Router();
 const Murottal = require('./Model');
 
+const listSurat = ["Al-Faatihah", "Al-Baqarah", "Ali-'Imraan", "An-Nisaa'", "Al-Maaidah", "Al-An'aam", "Al-A'raaf", "Al-Anfaal", "At-Taubah", "Yunus", "Hud", "Yusuf", "Ar-Ra'd", "Ibrahim", "Al-Hijr", "An-Nahl", "Al-Israa'", "Al-Kahfi", "Maryam", "Thaahaa", "Al-Anbiyaa'", "Al-Hajj", "Al-Mu'minuun", "An-Nuur", "Al-Furqaan", "Asy-Syu'araa'", "An-Naml", "Al-Qashash", "Al-'Ankabuut", "Ar-Ruum", "Luqman", "As-Sajdah", "Al-Ahzaab", "Saba'", "Faathir", "Yaa Siin", "Ash-Shaffaat", "Shaad", "Az-Zumar", "Ghafir", "Fushshilat", "Asy-Syuura", "Az-Zukhruf", "Ad-Dukhaan", "Al-Jaatsiyah", "Al-Ahqaaf", "Muhammad", "Al-Fath", "Al-Hujuraat", "Qaaf", "Adz-Dzaariyaat", "Ath-Thuur", "An-Najm", "Al-Qamar", "Ar-Rahmaan", "Al-Waaqi'ah", "Al-Hadiid", "Al-Mujaadilah", "Al-Hasyr", "Al-Mumtahanah", "Ash-Shaaf", "Al-Jumu'ah", "Al-Munaafiquun", "At-Taghaabun", "Ath-Thalaaq", "At-Tahriim", "Al-Mulk", "Al-Qalam", "Al-Haaqqah", "Al-Ma'aarij", "Nuh", "Al-Jin", "Al-Muzzammil", "Al-Muddatstsir", "Al-Qiyaamah", "Al-Insaan", "Al-Mursalaat", "An-Nabaa'", "An-Naazi'aat", "'Abasa", "At-Takwiir", "Al-Infithaar", "Al-Muthaffifiin", "Al-Insyiqaaq", "Al-Buruuj", "Ath-Thaariq", "Al-A'laa", "Al-Ghaasyiyah", "Al-Fajr", "Al-Balad", "Asy-Syams", "Al-Lail", "Adh-Dhuhaa", "Al-Insyiraah", "At-Tiin", "Al-'Alaq", "Al-Qadr", "Al-Bayyinah", "Al-Zalzalah", "Al-'Aadiyaat", "Al-Qaari'ah", "At-Takaatsur", "Al-'Ashr", "Al-Humazah", "Al-Fiil", "Quraisy", "Al-Maa'uun", "Al-Kautsar", "Al-Kaafiruun", "An-Nashr", "Al-Lahab", "Al-Ikhlaash", "Al-Falaq", "An-Naas"];
+
 /**
  * @swagger
  * /murottal/:
@@ -52,16 +54,12 @@ router.get('/', (req, res)=>{
  *       200:
  *         description: hasil
  */
-//TODO:: not finished
 router.get('/:name/:surat', (req, res)=>{
     let name = req.params.name;
     let surat = req.params.surat;
 
-    let q = Murottal.find({name: name});
-    q.where('likes').in(['name']);
-    q.exec((err, data) => {
+    Murottal.find({name: new RegExp(name, "i"), riwayat: /Ashim/}, (err, data)=>{
         if(err) return res.status(500).send(err);
-        console.log(data);
 
         if(_.size(data) > 1){
             let name = [];
@@ -77,18 +75,21 @@ router.get('/:name/:surat', (req, res)=>{
                     info: 'nama kurang spesifik'
                 });
         } else{
+            data = data[0];
             let arrAvailableSurat = data.listSurat.split(',');
 
-            if(_.findIndex(arrAvailableSurat, surat) !== -1){
+            if(_.includes(arrAvailableSurat, surat)){
                 res.json({
                     success: true,
                     name: data.name,
-                    url: data.url + '/' + ('000' + surat).substr(-3) + '/.mp3'
+                    surat: listSurat[surat-1],
+                    url: data.url + '/' + ('000' + surat).substr(-3) + '.mp3'
                 });
             } else{
                 res.json({
                     success: false,
                     name: data.name,
+                    listSurat: data.listSurat,
                     info: 'nr. surat tidak ditemukan'
                 });
             }
